8b01c7b39c1b430066c027cd3b67974c0d732865
zoilaz <zoila8314@gmail.com>
2019-10-11 11:29:30 +0300

    added 404, about, contacts

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,45 @@ $(function(){
     slidesToScroll: 4
   });
 
+  
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+});
+
+  $('.icon-list').on('click', function(){
+    $('.product__item').addClass('list');
+    $('.icon-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+
+  $('.icon-th-large').on('click', function(){
+    $('.product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-list').removeClass('active');
+  });
+  
+  $('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+  
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+
   var mixer = mixitup('.products__inner-box');
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,9 @@ gulp.task('style', function(){
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
-        'node_modules/rateyo/src/jquery.rateyo.css'
+        'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -35,7 +37,9 @@ gulp.task('script', function(){
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
-        'node_modules/rateyo/src/jquery.rateyo.js'
+        'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
